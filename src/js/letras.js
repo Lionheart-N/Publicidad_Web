cbd9171e4c0d7bc56a3c5d2d8593f18d6d68d79b
Lionheart-N <lnhgabrieldubuc@gmail.com>
2022-04-13 23:05:43 -0500

    Finalización del header

---
+++ src/js/letras.js
@@ -0,0 +1,13 @@
+const typed = new Typed('.typed',{
+    strings:[
+        '<i class="palabra">calle</i>',
+        '<i class="palabra">avenida</i>',
+        '<i class="palabra">lugar</i>'
+    ],
+    typeSpeed: 75,
+    startDelay: 300,
+    backSpeed: 75,
+    backDelay: 1500,
+    loop: true,
+    loopCount: false
+});
